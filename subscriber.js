5bc05fc5690a7cf174d0a65a30ed2dcda061a6b6
Daniel Henning <daniel@puresick.me>
2016-05-28 12:51:37 +0200

    Added console.log about subscribed topic

--- subscriber.js
+++ subscriber.js
@@ -1,8 +1,13 @@
 var mqtt = require('mqtt');
 var client = mqtt.connect('mqtt://139.59.144.75');
 
+var subscriptionTopic = process.argv[2];
+
 client.on('connect', function() {
-  client.subscribe('test');
+  console.log('Connection established');
+  console.log('Subscribed topic :' + subscriptionTopic); 
+
+  client.subscribe(subscriptionTopic);
 });
 
 client.on('message', function(topic, message) {
